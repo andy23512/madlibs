2adb708b320f16073658844eb46ec2709ca62fd6
andy23512 <andy23512@gmail.com>
2018-03-01 10:40:09 +0800

    add speech service to provider

--- madlibs-client/src/app/app.module.ts
+++ madlibs-client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SpeechService } from './speech.service';
 
 
 @NgModule({
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SpeechService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
