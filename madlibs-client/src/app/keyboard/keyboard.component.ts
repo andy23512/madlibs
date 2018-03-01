67d7b85aa9adf0d5dfe70f36b1399681ae5fb084
andy23512 <andy23512@gmail.com>
2018-03-02 00:46:21 +0800

    complete keyboard component

--- madlibs-client/src/app/keyboard/keyboard.component.ts
+++ madlibs-client/src/app/keyboard/keyboard.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-keyboard',
   templateUrl: './keyboard.component.pug',
   styleUrls: ['./keyboard.component.scss']
 })
-export class KeyboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class KeyboardComponent {
+  constructor() {}
 }
