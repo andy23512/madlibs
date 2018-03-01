c82646ebec04c89a8e3603d9b31ed8b1edb7485c
andy23512 <andy23512@gmail.com>
2018-03-01 23:38:35 +0800

    add madlibs service implementation, inject madlibs service in app.module

--- madlibs-client/src/app/app.module.ts
+++ madlibs-client/src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { SpeechService } from './speech.service';
 import { ListenComponent } from './listen/listen.component';
-
+import { MadlibsService } from './madlibs.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListenComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [
-    SpeechService
-  ],
+  declarations: [AppComponent, ListenComponent],
+  imports: [BrowserModule],
+  providers: [SpeechService, MadlibsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
