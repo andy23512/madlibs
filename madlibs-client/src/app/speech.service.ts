8787e1c1428d9c91e842d1a8933fd948c62d97ed
andy23512 <andy23512@gmail.com>
2018-03-01 10:57:29 +0800

    implement speech service

--- madlibs-client/src/app/speech.service.ts
+++ madlibs-client/src/app/speech.service.ts
@@ -1,8 +1,75 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+declare var annyang: any;
 
 @Injectable()
 export class SpeechService {
+  words$ = new Subject<{ [key: string]: string }>();
+  errors$ = new Subject<{ [key: string]: any }>();
+  listening = false;
 
-  constructor() { }
+  constructor(private zone: NgZone) {}
 
+  get speechSupported(): boolean {
+    return !!annyang;
+  }
+
+  init() {
+    const commands = {
+      'noun : noun': noun => {
+        this.zone.run(() => {
+          this.words$.next({ type: 'noun', word: noun });
+        });
+      },
+      'verb :verb': verb => {
+        this.zone.run(() => {
+          this.words$.next({ type: 'verb', word: verb });
+        });
+      },
+      'adjective :adj': adj => {
+        this.zone.run(() => {
+          this.words$.next({ type: 'adj', word: adj });
+        });
+      }
+    };
+    annyang.addCommands(commands);
+    annyang.addCallback('errorNetwork', err => {
+      this._handleError('network', 'A network error occurred', err);
+    });
+    annyang.addCallback('errorPermissionBlocked', err => {
+      this._handleError(
+        'blocked',
+        'Browser blocked microphone permissions',
+        err
+      );
+    });
+    annyang.addCallback('resultNoMatch', userSaid => {
+      this._handleError(
+        'no match',
+        'Spoken command not recognized. Say "noun [word]", "verb [word]", OR "adjective [word]".',
+        { results: userSaid }
+      );
+    });
+  }
+
+  private _handleError(error, msg, errObj) {
+    this.zone.run(() => {
+      this.errors$.next({
+        error: error,
+        message: msg,
+        obj: errObj
+      });
+    });
+  }
+
+  startListening() {
+    annyang.start();
+    this.listening = true;
+  }
+
+  abort() {
+    annyang.abort();
+    this.listening = false;
+  }
 }
