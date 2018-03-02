c52b161cc3229a834e3b51d6f17af31716dedcc3
andy23512 <andy23512@gmail.com>
2018-03-02 20:31:02 +0800

    add progress-bar component implement

--- madlibs-client/src/app/progress-bar/progress-bar.component.ts
+++ madlibs-client/src/app/progress-bar/progress-bar.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/takeUntil';
+import { MadlibsService } from '../madlibs.service';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.pug',
   styleUrls: ['./progress-bar.component.scss']
 })
-export class ProgressBarComponent implements OnInit {
+export class ProgressBarComponent implements OnInit, OnDestroy {
+  progress = 0;
+  progress$: Observable<number>;
+  progressSub: Subscription;
+  width: string;
+  submitSub: Subscription;
+  pronounSub: Subscription;
 
-  constructor() { }
+  constructor(private ml: MadlibsService) {}
 
   ngOnInit() {
+    this._setupProgress();
+    this._setupSubmit();
   }
 
+  private _setupProgress() {
+    this.progress$ = Observable.timer(0, 50).takeUntil(Observable.timer(2850));
+  }
+
+  private _getPronoun() {
+    this.pronounSub = this.ml
+      .getPronoun$()
+      .subscribe(res => this.ml.setPronoun(res));
+  }
+
+  private _setupSubmit() {
+    this.submitSub = this.ml.submit$.subscribe(words => this._startProgress());
+  }
+
+  private _startProgress() {
+    this._getPronoun();
+    this.progressSub = this.progress$.subscribe(
+      p => {
+        this.progress = p * 2;
+        this.width = this.progress + '%';
+      },
+      err => console.warn('Progress error:', err),
+      () => this.ml.setMadlibReady(true)
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.progressSub) {
+      this.progressSub.unsubscribe();
+    }
+    if (this.pronounSub) {
+      this.pronounSub.unsubscribe();
+    }
+    this.submitSub.unsubscribe();
+  }
 }
