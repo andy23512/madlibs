32a1d9a9da2e8c4e952112a28224da057df12464
andy23512 <andy23512@gmail.com>
2018-03-02 20:08:38 +0800

    implement generate words component

--- madlibs-client/src/app/generate-words/generate-words.component.ts
+++ madlibs-client/src/app/generate-words/generate-words.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  OnDestroy
+} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { MadlibsService } from '../madlibs.service';
 
 @Component({
   selector: 'app-generate-words',
   templateUrl: './generate-words.component.pug',
   styleUrls: ['./generate-words.component.scss']
 })
-export class GenerateWordsComponent implements OnInit {
+export class GenerateWordsComponent implements OnDestroy {
+  @Output() fetchedWords = new EventEmitter(); // emit an event from this component to a parent
+  wordsSub: Subscription;
+  loading = false;
+  generated = false;
+  error = false;
 
-  constructor() { }
+  constructor(private ml: MadlibsService) {}
 
-  ngOnInit() {
+  fetchWords() {
+    this.loading = true;
+    this.generated = false;
+    this.error = false;
+
+    this.wordsSub = this.ml.getWords$().subscribe(
+      res => {
+        this.loading = false;
+        this.generated = true;
+        this.error = false;
+        this.fetchedWords.emit(res);
+      },
+      err => {
+        this.loading = false;
+        this.generated = false;
+        this.error = true;
+        console.warn(err);
+      }
+    );
   }
 
+  ngOnDestroy() {
+    if (this.wordsSub) {
+      this.wordsSub.unsubscribe();
+    }
+  }
 }
